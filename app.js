114adb0f7b8d6a433de795d2f4947d5ebebd4eae
Anna Trzeciak <trzeciakanna@hotmail.com>
2020-01-26 11:52:00 +0100

    Created main page on server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const mainRoutes = require("./routes/main");
 const productsRoutes = require("./routes/products");
 
 const app = express();
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", mainRoutes);
 app.use("/api/products", productsRoutes);
 
 module.exports = app;
