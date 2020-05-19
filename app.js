7576bd5d657d0b166ed05e2a735a12ded62958de
Anna Trzeciak <anna.trzeciak@editshare.com>
2020-05-19 23:11:04 +0200

    Corrected eslint file, added cors

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const mainRoutes = require('./routes/main');
-const productsRoutes = require('./routes/products');
-const detailsRoutes = require('./routes/details');
-const userRoutes = require('./routes/user');
+const mainRoutes = require("./routes/main");
+const productsRoutes = require("./routes/products");
+const detailsRoutes = require("./routes/details");
+const userRoutes = require("./routes/user");
 
-const config = require('./config');
+const config = require("./config");
 
 // const test = require('./routes/tests');
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(config.URI_MONGO, {
-  useCreateIndex: true,
-  useNewUrlParser: true
-}).catch((err) => console.log('Error: Could not connect to MongoDB.', err));
+mongoose
+  .connect(config.URI_MONGO, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  })
+  .catch(err => console.log("Error: Could not connect to MongoDB.", err));
 
-
-mongoose.connection.on('connected', () => {
-  console.log('Connected to database');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database");
 });
-mongoose.connection.on('error', (err) => {
-  console.log('Error: Could not connect to MongoDB.', err);
+mongoose.connection.on("error", err => {
+  console.log("Error: Could not connect to MongoDB.", err);
 });
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 
-app.use('/', mainRoutes);
-app.use('/api/products', productsRoutes);
-app.use('/api/details', detailsRoutes);
-app.use('/api/users', userRoutes);
+app.use("/", mainRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/details", detailsRoutes);
+app.use("/api/users", userRoutes);
 // app.use('/api/tests', test);
 
 module.exports = app;
